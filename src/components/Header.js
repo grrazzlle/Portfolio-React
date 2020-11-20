da4d8f92290083e3e71ea8a380953782a1ef8df2
grrazzlle <cel47@miami.edu>
2020-11-19 23:55:13 -0500

    fix 6

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom';
 
 class Header extends Component {
   state = {
-    currentPage: "AboutPage"
+    currentPage: "/Portfolio-React/AboutPage"
   };
 
   handlePageChange = page => {
@@ -16,10 +16,10 @@ class Header extends Component {
 
   renderPage = () => {
     return <Switch>
-      <Route exact path="ContactPage" component={ContactPage} />
-      <Route exact path="AboutPage" component={AboutPage} />
-      <Route exact path="PortfolioPage" component={PortfolioPage} />
-      <Redirect to="AboutPage" />
+      <Route exact path="/Portfolio-React/ContactPage" component={ContactPage} />
+      <Route exact path="/Portfolio-React/AboutPage" component={AboutPage} />
+      <Route exact path="/Portfolio-React/PortfolioPage" component={PortfolioPage} />
+      <Redirect to="/Portfolio-React/AboutPage" />
     </Switch>
 
   };
